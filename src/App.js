b976e923b7942ba768b67cb2846195e282325868
yigitertor <yigitertor@hotmail.com>
2024-08-14 16:29:00 +0300

    Added FormAddFriend and FriendList functions

--- src/App.js
+++ src/App.js
@@ -1,20 +1,84 @@
 const initialFriends = [
-  {
-    id: 118836,
-    name: "Clark",
-    image: "https://i.pravatar.cc/48?u=118836",
-    balance: -7,
-  },
-  {
-    id: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
-    balance: 20,
-  },
-  {
-    id: 499476,
-    name: "Anthony",
-    image: "https://i.pravatar.cc/48?u=499476",
-    balance: 0,
-  },
+   {
+      id: 118836,
+      name: "Clark",
+      image: "https://i.pravatar.cc/48?u=118836",
+      balance: -7,
+   },
+   {
+      id: 933372,
+      name: "Sarah",
+      image: "https://i.pravatar.cc/48?u=933372",
+      balance: 20,
+   },
+   {
+      id: 499476,
+      name: "Anthony",
+      image: "https://i.pravatar.cc/48?u=499476",
+      balance: 0,
+   },
 ];
+
+export default function App() {
+   return (
+      <div className="app">
+         <div className="sidebar">
+            <FriendsList />
+            <Button>Add Friend</Button>
+            <FormAddFriend />
+         </div>
+      </div>
+   );
+}
+
+function FriendsList() {
+   const friends = initialFriends;
+   return (
+      <ul>
+         {friends.map((friend) => (
+            <Friend friend={friend} key={friend.id} />
+         ))}
+      </ul>
+   );
+}
+
+function Friend({ friend }) {
+   return (
+      <li>
+         <img src={friend.image} alt={friend.name} />
+         <h3>{friend.name}</h3>
+
+         {friend.balance < 0 && (
+            <p className="red">
+               You owe {friend.name} {Math.abs(friend.balance)}$
+            </p>
+         )}
+         {friend.balance > 0 && (
+            <p className="green">
+               You owe {friend.name} {Math.abs(friend.balance)}$
+            </p>
+         )}
+         {friend.balance === 0 && <p>You and {friend.name} are even.</p>}
+
+         <Button />
+      </li>
+   );
+}
+
+function Button({ children }) {
+   return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+   return (
+      <form className="form-add-friend">
+         <label>🧑🏻‍🤝‍👩🏼Friend Name</label>
+         <input type="text"></input>
+
+         <label>🖼️ Image URL</label>
+         <input type="text" />
+
+         <button className="button">Select</button>
+      </form>
+   );
+}
